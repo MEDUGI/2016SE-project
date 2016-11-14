a89c032ec2d51c61f453289adf151917a31293cf
tzjtatata <tzjtatata@qq.com>
2016-11-14 21:58:35 +0800

    Finished ApplicationDAO and RelationDAO

--- src/dao/ApplicationDAO.java
+++ src/dao/ApplicationDAO.java
@@ -19,9 +19,8 @@ public class ApplicationDAO {
             ps.setString(1,stuID);
             ps.setString(2,proID);
             ps.setString(3,date);
-            ps.setString(4,date);
-            ps.setString(5,status);
-            ps.setString(6,message);
+            ps.setString(4,status);
+            ps.setString(5,message);
             i = ps.executeUpdate();
             ps.close();
         }catch (Exception e) {
@@ -29,23 +28,36 @@ public class ApplicationDAO {
         }
         return i;
     }
-
-    public ArrayList<Application> findAllApplicationByStudent(String stuid) {
+    // mode = 1, id belongs to student. mode = 0, id belongs to tutor.
+    public ArrayList<Application> findAllApplicationByStudent(String ID, int mode) {
         int i = 0;
-        String sql = "select * from application when studentid=?";
+        String sql = "";
+        if (mode == 0) {
+            sql = "select * from application when professorID=?";
+        }
+        else {
+            sql = "select * from application when studentID=?";
+        }
+        ArrayList<Application> result = new ArrayList<Application>();
         try{
             ps = dbp.getConn().prepareStatement(sql);
-            ps.setString(1,stuid);
+            ps.setString(1, ID);
             i = ps.executeUpdate();
             ResultSet rs = ps.executeQuery();
             while(rs.next()) {
-
+                Application Temp = new Application();
+                Temp.setStu(rs.getString("student"));
+                Temp.setPro(rs.getString("professor"));
+                Temp.setApplydate(rs.getString("date"));
+                Temp.setStatus(Integer.parseInt(rs.getString("status")));
+                Temp.setMessage(rs.getString("message"));
+                result.add(Temp);
             }
             ps.close();
         }catch (Exception e) {
             e.printStackTrace();
         }
-        return i;
+        return result;
     }
 
     public int updateStatus(String appid,String newstatus) {
@@ -55,6 +67,20 @@ public class ApplicationDAO {
             ps = dbp.getConn().prepareStatement(sql);
             ps.setString(1,newstatus);
             ps.setString(2,appid);
+            i = ps.executeUpdate();
+            ps.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return i;
+    }
+
+    public int deleteStatus(String appid) {
+        int i = 0;
+        String sql = "delete from application when id=?";
+        try{
+            ps = dbp.getConn().prepareStatement(sql);
+            ps.setString(1, appid);
             i = ps.executeUpdate();
             ps.close();
         }catch (Exception e) {
