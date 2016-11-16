d376e82b37662127bd3332ea585e1c4fafddbfe6
tzjtatata <tzjtatata@qq.com>
2016-11-16 16:38:20 +0800

    Update the recommand.jsp

--- src/action/recommand.java
+++ src/action/recommand.java
@@ -6,19 +6,25 @@ import entity.RecommandProfessor;
 import entity.RecommandStudent;
 import entity.Tools;
 
+import java.util.ArrayList;
+
 /**
  * Created by 李沅泽 on 2016/11/16.
  */
 public class recommand extends ActionSupport{
     private String user, userstyle;
+    private ArrayList result;
     public String execute() {
         if (Tools.examUserWithStyle(user,userstyle)) {
             if (userstyle == "Student") {
                 RecommandStudent recs = new RecommandStudent();
-
+                result = recs.execWithRestraint();
+                return "SUCCESS";
             }
             else {
                 RecommandProfessor recp = new RecommandProfessor();
+                result = recp.execWithRestraint();
+                return "SUCCESS";
             }
         }
         else return "USER_NOT_FOUND";
