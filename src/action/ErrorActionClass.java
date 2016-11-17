4a5c75b836341e796c0f01c0ff2ddaca80184390
ZhangXiangxi <zxx_1996@qq.com>
2016-11-18 04:09:04 +0800

    1. Changed various error handling jsp pages into a session based action. 2. Redirect the index.action to loggin.action;

---
+++ src/action/ErrorActionClass.java
@@ -0,0 +1,42 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
+
+/**
+ * Created by Xiangxi on 2016/11/18.
+ */
+public class ErrorActionClass extends ActionSupport implements SessionAware{
+    Map session;
+    String errorMessage;
+
+    public Map getSession() {
+        return session;
+    }
+
+    public void setSession(Map session) {
+        this.session = session;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        if (session.containsKey("errorMessage")) {
+            errorMessage = (String)session.get("errorMessage");
+            session.remove("errorMessage");
+            return SUCCESS;
+        } else {
+            errorMessage = "我们不知道是什么把您带到了错误界面；我们没有检测到任何错误，而这可能是最大的错误。";
+            return SUCCESS;
+        }
+    }
+}
--- web/error.jsp
+++ web/error.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: zxx_1
@@ -11,7 +12,7 @@
     <title>呃唔，出错了。。。</title>
 </head>
 <body>
-出现了一个未知的错误，你现在处在互联网的荒野。<br>
+<s:property value="errorMessage"/> <br>
 <button onclick="window.open('/login_normal.jsp')">
   回到登录界面
 </button>
