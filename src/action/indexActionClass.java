1d5c7272861b972f76c28db762d3f5eeb26d3fa4
ZhangXiangxi <zxx_1996@qq.com>
2016-11-18 04:57:51 +0800

    1. make the mainpage more smart. 2. simplify the change_passwordAction with session

---
+++ src/action/indexActionClass.java
@@ -0,0 +1,28 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
+
+/**
+ * Created by Xiangxi on 2016/11/18.
+ */
+public class indexActionClass extends ActionSupport implements SessionAware{
+    Map session;
+
+    public Map getSession() {
+        return session;
+    }
+
+    public void setSession(Map session) {
+        this.session = session;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        if (session.containsKey("username"))
+            return "logged";
+        return "unlogged";
+    }
+}
